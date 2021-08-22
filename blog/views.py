524293a9defa3465b11a415a37d2ff661286c0f2
Ronith <44728044+Ronith900@users.noreply.github.com>
2021-08-22 20:29:43 +0800

    new templates commit

--- blog/views.py
+++ blog/views.py
@@ -3,5 +3,28 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+posts = [
+    {
+        'author': 'CoreyMS',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 28, 2018'
+    }
+]
+
+
 def home(request):
-    return HttpResponse("<h1>Hello</h1>")
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About'})

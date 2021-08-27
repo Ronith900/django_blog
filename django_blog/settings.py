bc86d885c72b793715b362f7aacfab40cfa97bee
Ronith <44728044+Ronith900@users.noreply.github.com>
2021-08-27 11:23:36 +0800

    env vairbale

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -11,7 +11,12 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-import os
+import os, environ
+
+env = environ.Env()
+
+environ.Env.read_env()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0(&5aof81bc6_)mee5#(3)j4@&kw*x16fm!^30olq39byeppr4'
+SECRET_KEY = env['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env['DEBUG']
 
 ALLOWED_HOSTS = []
 
